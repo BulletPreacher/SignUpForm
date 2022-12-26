297de1afd34e597e28b0c9caa663ae8718ccfe27
Lyle <Lyle.strydom96@gmail.com>
2022-12-26 17:27:36 +0200

    Create landing page, form and validation

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("myForm");
+const btn= document.getElementById("btnSubmit")
+
+function verifyPass() {
+    var pass1 = document.getElementById("user_password1").value;
+    var pass2 = document.getElementById("user_password2").value;
+    if (pass1 !== pass2) {
+        console.log("wrong")
+        console.log(pass1);
+        console.log(pass2);
+        document.getElementById("user_password2").setCustomValidity("Passwords do not match");
+        document.getElementById("user_password2").reportValidity();
+        return false;
+    }
+    return true;
+}
